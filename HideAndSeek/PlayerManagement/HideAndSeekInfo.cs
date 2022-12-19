f93d201f5dfa5f6d1e79136c02088fcf04109ff8
Locochoco <ivanrwpf@gmail.com>
2022-12-19 17:17:40 -0300

    fixed roles (?)

--- HideAndSeek/PlayerManagement/HideAndSeekInfo.cs
+++ HideAndSeek/PlayerManagement/HideAndSeekInfo.cs
@@ -8,6 +8,8 @@ namespace HideAndSeek{
         public AudioSignal signal;
         public PlayerState state;
 
+        public bool isSetup;
+
         public virtual void SetupSeeker(){
             if (this.state == PlayerState.Seeking){
                 Utils.WriteLine(this.playerInfo + " is already a Seeker", MessageType.Info);
@@ -24,26 +26,28 @@ namespace HideAndSeek{
                 return;
             }
 
-            GameObject seekerVolume = new("seeker_volume");
-            seekerVolume.transform.parent = this.playerInfo.Body.transform;
-            seekerVolume.transform.localPosition = Vector3.zero;
-            seekerVolume.transform.localRotation = Quaternion.identity;
-            seekerVolume.AddComponent<SeekerTrigger>();
-            
-            Utils.WriteLine("Adding the HUD Marker", MessageType.Success);
+            HideAndSeek.instance.ModHelper.Events.Unity.RunWhen(() => this.playerInfo.Body != null && isSetup, () => {            
+                GameObject seekerVolume = new("seeker_volume");
+                seekerVolume.transform.parent = this.playerInfo.Body.transform;
+                seekerVolume.transform.localPosition = Vector3.zero;
+                seekerVolume.transform.localRotation = Quaternion.identity;
+                seekerVolume.AddComponent<SeekerTrigger>();
 
-            //We are the local player at this point
-            //We want to add all the HUD markers
-            //of all the seekers
-            //foreach (var playerInfo in seekers){
-            this.playerInfo.HudMarker.enabled = true;
-            this.playerInfo.MapMarker.enabled = true;
-            //}
-            //foreach (var playerInfo in hiders)
-            //{
-            //    playerInfo.HudMarker.enabled = false;
-            //    playerInfo.MapMarker.enabled = false;
-            //}
+                Utils.WriteLine("Adding the HUD Marker", MessageType.Success);
+
+                //We are the local player at this point
+                //We want to add all the HUD markers
+                //of all the seekers
+                //foreach (var playerInfo in seekers){
+                this.playerInfo.HudMarker.enabled = true;
+                this.playerInfo.MapMarker.enabled = true;
+                //}
+                //foreach (var playerInfo in hiders)
+                //{
+                //    playerInfo.HudMarker.enabled = false;
+                //    playerInfo.MapMarker.enabled = false;
+                //}
+            });
         }
 
         public virtual void SetupHider(){
@@ -64,11 +68,13 @@ namespace HideAndSeek{
                 return;
             }
 
-            this.signal._sourceRadius = 500; //Magic OoOOooOh (Around Timber Hearth Radius)
-            Utils.WriteLine("Removing the HUD Marker", MessageType.Success);
+            HideAndSeek.instance.ModHelper.Events.Unity.RunWhen(() => this.playerInfo.Body != null && isSetup, () =>{
+                this.signal._sourceRadius = 500; //Magic OoOOooOh (Around Timber Hearth Radius)
+                Utils.WriteLine("Removing the HUD Marker", MessageType.Success);
 
-            //this.playerInfo.HudMarker.enabled = false;
-            //this.playerInfo.MapMarker.enabled = false;
+                this.playerInfo.MapMarker.enabled = false;
+                this.playerInfo.HudMarker.enabled = false;
+            });            
         }
 
         public virtual void SetupSpectator(){
