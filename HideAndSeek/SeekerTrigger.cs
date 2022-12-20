3f9918b0ab702fe013ef6fb83beee0d0c0ffd866
Locochoco <ivanrwpf@gmail.com>
2022-12-19 22:57:14 -0300

    Fixed missing event unsubscribe plus added caught sound

--- HideAndSeek/SeekerTrigger.cs
+++ HideAndSeek/SeekerTrigger.cs
@@ -12,12 +12,14 @@ namespace HideAndSeek{
         public PlayerInfo seekerInfo;
         
         public void Start(){
-            CapsuleShape shapeTrigger = gameObject.AddComponent<CapsuleShape>();
-            shapeTrigger.radius = 0.5f;
-            shapeTrigger.height = 2f;
-            shapeTrigger.SetCollisionMode(Shape.CollisionMode.Volume);
 
-            triggerVolume = gameObject.AddComponent<OWTriggerVolume>();
+            if (GetComponent<CapsuleShape>() == null){
+                CapsuleShape shapeTrigger = gameObject.AddComponent<CapsuleShape>();
+                shapeTrigger.radius = 0.5f;
+                shapeTrigger.height = 2f;
+                shapeTrigger.SetCollisionMode(Shape.CollisionMode.Volume);
+            }
+            triggerVolume = gameObject.GetAddComponent<OWTriggerVolume>();
             triggerVolume.OnEntry += ShapeTrigger_OnEntry;
         }
 
@@ -29,9 +31,13 @@ namespace HideAndSeek{
             {
                 //TODO :: ADD CUSTOM DEATHTYPES
                 Locator.GetDeathManager().KillPlayer(DeathType.CrushedByElevator);
-                new RoleChangeMessage(seekerInfo.PlayerId, PlayerState.Hiding).Send();
+                Locator.GetPlayerAudioController().PlayOneShotInternal(AudioType.Death_Crushed);
                 StartCoroutine(AutoRespawnWithDelay(5f));
             }
+        }
+
+        private void OnDestroy(){
+            triggerVolume.OnEntry -= ShapeTrigger_OnEntry;
         }
 
         IEnumerator AutoRespawnWithDelay(float delay)
@@ -39,6 +45,7 @@ namespace HideAndSeek{
             yield return new WaitForSeconds(delay);
             new LocationRespawnMessage(QSBPlayerManager.LocalPlayerId, SpawnLocation.TimberHearth).Send();
             new RoleChangeMessage(QSBPlayerManager.LocalPlayerId, PlayerState.Seeking).Send();
+            new RoleChangeMessage(seekerInfo.PlayerId, PlayerState.Hiding).Send();
         }
 
     }
