bb87b97fbdec98e9958cfc97e89f07d0cec3e224
BUNN1E5 <jgneedham@epsilonsystems.com>
2022-12-19 16:18:13 -0800

    Moved to local implemntation of RunWhen

--- HideAndSeek/Utils.cs
+++ HideAndSeek/Utils.cs
@@ -1,4 +1,8 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using OWML.Common;
+using UnityEngine;
 
 namespace HideAndSeek{
     public static class Utils {
@@ -6,6 +10,15 @@ namespace HideAndSeek{
         public static IModUnityEvents Unity = HideAndSeek.instance.ModHelper.Events.Unity; 
         public static void WriteLine(string s, MessageType t){
             HideAndSeek.instance.ModHelper.Console.WriteLine(s, t);
+        }
+        
+        public static Coroutine RunWhen(Func<bool> predicate, Action action) => 
+            HideAndSeek.instance.StartCoroutine(WaitUntil(predicate, action));
+
+        private static IEnumerator WaitUntil(Func<bool> predicate, Action action)
+        {
+            yield return new WaitUntil(predicate);
+            action();
         }
         
     }
