293008ebe76c47841831110cccbbc31d3908cddf
Jere Joensuu <jerepetteri.joensuu@gmail.com>
2025-03-18 19:12:06 +0200

    Cleanup code

--- QSBGameModes/Menu/GameModeMenu.cs
+++ QSBGameModes/Menu/GameModeMenu.cs
@@ -7,26 +7,31 @@ using QSBGameModes.GameManagement.PlayerManagement;
 
 namespace QSBGameModes.Menu;
 
-public static class GameModeMenu{
-    
-    private static SubmitAction HostButton;
-    private static SubmitAction JoinButton;
+public static class GameModeMenu
+{
+    private static SubmitAction _hostButton;
+    private static SubmitAction _joinButton;
 
-    public static void SetupPauseButton(){
+    public static void SetupPauseButton()
+    {
         Utils.WriteLine("Adding button to menu", MessageType.Info);
-        if (QSBCore.IsHost){
-            HostButton = QSBGameModes.instance.ModHelper.MenuHelper.PauseMenuManager.MakeSimpleButton("READY UP", 0, false);
-            HostButton.OnSubmitAction += HostButtonHandler;
+        if (QSBCore.IsHost)
+        {
+            _hostButton =
+                QSBGameModes.instance.ModHelper.MenuHelper.PauseMenuManager.MakeSimpleButton("READY UP", 0, false);
+            _hostButton.OnSubmitAction += HostButtonHandler;
         }
 
-        JoinButton = QSBGameModes.instance.ModHelper.MenuHelper.PauseMenuManager.MakeSimpleButton("JOIN GAME", 0, false);
-        JoinButton.OnSubmitAction += JoinButtonHandler;
+        _joinButton =
+            QSBGameModes.instance.ModHelper.MenuHelper.PauseMenuManager.MakeSimpleButton("JOIN GAME", 0, false);
+        _joinButton.OnSubmitAction += JoinButtonHandler;
 
         UpdateGUI();
     }
 
     //Update GUI gets ran so often LMAO
-    public static void UpdateGUI(){
+    public static void UpdateGUI()
+    {
         Utils.RunWhen(() => QSBWorldSync.AllObjectsReady,
             () => Utils.ModHelper.Events.Unity.FireInNUpdates(UpdateGUI_, 10));
     }
@@ -40,50 +45,55 @@ public static class GameModeMenu{
         Utils.WriteLine($"Updating GUI, Current State is {GameManager.state.GetName()}");
         Utils.WriteLine($"Current Player state is {currentPlayerState}");
 
-        
-        if (QSBCore.IsHost){
+        if (QSBCore.IsHost)
+        {
             UpdateHostButton();
         }
 
         UpdateJoinButton(currentPlayerState);
     }
 
-    private static void UpdateHostButton(){
+    private static void UpdateHostButton()
+    {
         if (GameManager.state == GameState.Stopped)
-            ChangeButtonText(HostButton, "START " + SharedSettings.settingsToShare.GameType);
+            ChangeButtonText(_hostButton, "START " + SharedSettings.settingsToShare.GameType);
         else
-            ChangeButtonText(HostButton, "STOP " + SharedSettings.settingsToShare.GameType);
+            ChangeButtonText(_hostButton, "STOP " + SharedSettings.settingsToShare.GameType);
     }
 
-    private static void UpdateJoinButton(GameManagement.PlayerManagement.PlayerState currentPlayerState){
-        switch (currentPlayerState){
+    private static void UpdateJoinButton(GameManagement.PlayerManagement.PlayerState currentPlayerState)
+    {
+        switch (currentPlayerState)
+        {
             case GameManagement.PlayerManagement.PlayerState.Seeking:
             case GameManagement.PlayerManagement.PlayerState.Ready:
             case GameManagement.PlayerManagement.PlayerState.Hiding:
                 if (GameManager.state != GameState.Stopped)
-                    ChangeButtonText(JoinButton, "SPECTATE " + SharedSettings.settingsToShare.GameType);
+                    ChangeButtonText(_joinButton, "SPECTATE " + SharedSettings.settingsToShare.GameType);
                 else
-                    ChangeButtonText(JoinButton, "UNREADY UP FOR " + SharedSettings.settingsToShare.GameType);
+                    ChangeButtonText(_joinButton, "UNREADY UP FOR " + SharedSettings.settingsToShare.GameType);
                 break;
             case GameManagement.PlayerManagement.PlayerState.None:
             case GameManagement.PlayerManagement.PlayerState.Spectating:
             default:
                 if (GameManager.state != GameState.Stopped)
-                    ChangeButtonText(JoinButton, "JOIN " + SharedSettings.settingsToShare.GameType);
+                    ChangeButtonText(_joinButton, "JOIN " + SharedSettings.settingsToShare.GameType);
                 else
-                    ChangeButtonText(JoinButton, "READY UP FOR " + SharedSettings.settingsToShare.GameType);
+                    ChangeButtonText(_joinButton, "READY UP FOR " + SharedSettings.settingsToShare.GameType);
                 break;
         }
     }
 
     // Unified handler for the host button
-    private static void HostButtonHandler(){
+    private static void HostButtonHandler()
+    {
         if (GameManager.state == GameState.Stopped) QSBGameModes.StartGameMode();
         else QSBGameModes.StopGameMode();
     }
 
     // Unified handler for the join button
-    private static void JoinButtonHandler(){
+    private static void JoinButtonHandler()
+    {
         var currentPlayerState = PlayerManager.LocalPlayer == null
             ? GameManagement.PlayerManagement.PlayerState.None
             : PlayerManager.LocalPlayer.State;
@@ -103,7 +113,8 @@ public static class GameModeMenu{
         }
     }
 
-    private static void ChangeButtonText(SubmitAction button, string text){
+    private static void ChangeButtonText(SubmitAction button, string text)
+    {
         QSBGameModes.instance.ModHelper.MenuHelper.PauseMenuManager.SetButtonText(button, text);
     }
 }
